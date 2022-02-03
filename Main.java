993648cd310fff505a63f37bee2fee9e94b89fe0
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-02-03 00:25:08 -0500

    First commit.

---
+++ Main.java
@@ -0,0 +1,41 @@
+/*Create a random 2000-digit number and store it in a file. Read the number from the file and find the smallest number within our larger number that contains at least every digit. 
+For instance,149485420251367 contains every digit, but 94556780231 is a smaller number that contains every digit.*/
+
+import java.math.BigInteger;
+import java.util.Random;   
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+   
+   
+        BigInteger hugeNum = getBigInt();
+   String stringTheBig=hugeNum.toString();
+   
+       
+        
+        PrintWriter detail = null;
+        try {
+           detail = new PrintWriter(stringTheBig);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+     detail.close();
+ System.out.println(hugeNum);
+    }
+    
+    public static BigInteger getBigInt() {
+        Random rand = new Random();
+        
+        BigInteger result =  new BigInteger(4,rand);
+     result =  result.pow(2000);
+        return result;
+    }
+    
+}
+    
+
